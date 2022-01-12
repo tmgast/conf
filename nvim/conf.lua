2dbbdfc81eca9131063d3cd826025682d685bb35
Terri <tmgast@gmail.com>
2022-01-12 16:35:58 +0900

    added volar for vue3

--- nvim/conf.lua
+++ nvim/conf.lua
@@ -48,8 +48,11 @@ cmp.setup.cmdline(':', {
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 require('lspconfig')['tsserver'].setup {
-  capabilities = capabilities
+  capabilities = capabilities,
 }
 require('lspconfig')['vuels'].setup {
-  capabilities = capabilities
+  capabilities = capabilities,
+}
+require('lspconfig')['volar'].setup {
+  capabilities = capabilities,
 }
