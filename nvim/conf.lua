22d3434882783cb6f4a94cb68b5c19be4297cfc7
Terri <tmgast@gmail.com>
2022-01-14 16:17:35 +0900

    LSP update

--- nvim/conf.lua
+++ nvim/conf.lua
@@ -47,10 +47,10 @@ cmp.setup.cmdline(':', {
 -- Setup lspconfig.
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-require('lspconfig')['tsserver'].setup {
+require('lspconfig')['html'].setup {
   capabilities = capabilities,
 }
-require('lspconfig')['vuels'].setup {
+require('lspconfig')['tsserver'].setup {
   capabilities = capabilities,
 }
 require('lspconfig')['volar'].setup {
