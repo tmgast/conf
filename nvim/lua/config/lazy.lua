e978a56164b4d6cbc0cd7cefef2e764fd34817c4
Terri Gast <tmgast@gmail.com>
2024-11-14 13:58:20 +0900

    update copilot helper path

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -12,7 +12,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
-    { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.ai.copilot" },
     { import = "plugins" },
     { "echasnovski/mini.pairs", enabled = false },
   },
