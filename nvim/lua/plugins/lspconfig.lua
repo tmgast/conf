8cf5ff272b41120dbb26f6c4c7eac89669805c47
Terri Gast <tmgat@gmail.com>
2025-07-29 15:21:59 +0900

    correct LSP config options to work as intended

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,5 +1,31 @@
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.offsetEncoding = { "utf-16" }
+
+local filter_dts = function(err, result, ctx, config)
+  if not result then
+    return
+  end
+  -- turn single Location into a list, so we can filter uniformly
+  local is_list = vim.lsp.util.is_list(result)
+  local items = is_list and result or { result }
+
+  -- drop anything whose URI ends in .d.ts
+  local filtered = vim.tbl_filter(function(item)
+    return not item.uri:match("%.d.ts$")
+  end, items)
+
+  -- if nothing left, fall back to the original (so you still jump somewhere)
+  local to_send = #filtered > 0 and filtered or items
+
+  -- if it was a single Location originally, unwrap it
+  if not is_list then
+    to_send = to_send[1]
+  end
+
+  -- forward to the default handler
+  vim.lsp.handlers["textDocument/definition"](err, to_send, ctx, config)
+end
+
 return {
   {
     "saghen/blink.cmp",
@@ -17,17 +43,21 @@ return {
       opts.completion = opts.completion or {}
       opts.completion.list = opts.completion.list or {}
       opts.completion.list.selection = {
-        preselect = false, -- Do not preselect
-        auto_insert = false, -- Do not auto insert
+        preselect = false,
+        auto_insert = false,
       }
+
       return opts
     end,
   },
+
   {
     "echasnovski/mini.pairs",
     enabled = false,
   },
+
   { "SmiteshP/nvim-navic" },
+
   {
     "nvimtools/none-ls.nvim",
     optional = true,
@@ -35,61 +65,93 @@ return {
       local nls = require("null-ls")
       opts.sources = opts.sources or {}
       table.insert(opts.sources, nls.builtins.formatting.black)
+      return opts
     end,
   },
+
   {
     "stevearc/conform.nvim",
     optional = true,
     opts = {
       formatters_by_ft = {
-        ["python"] = { "ruff format %" },
+        python = { "ruff format %" },
       },
     },
   },
+  {},
   {
     "neovim/nvim-lspconfig",
     event = { "BufReadPre", "BufNewFile" },
     dependencies = {
-      { "folke/neoconf.nvim", cmd = "Neoconf", config = true },
-      { "folke/neodev.nvim", enabled = false, opts = { experimental = { pathStrict = true } } },
-      { "mason.nvim" },
-      { "williamboman/mason-lspconfig.nvim" },
+      {
+        "folke/neoconf.nvim",
+        cmd = "Neoconf",
+        opts = {},
+      },
+      {
+        "folke/neodev.nvim",
+        enabled = false,
+        opts = { experimental = { pathStrict = true } },
+      },
+      "mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
     },
     opts = function(_, opts)
-      local util = require("lspconfig.util")
-
-      -- helper: detect Vue/Nuxt tree by dev-config files
-      local function is_vue_project(fname)
-        return util.root_pattern("nuxt.config.ts", "nuxt.config.js", "vue.config.js")(fname)
-      end
-
-      -- make sure we have a setup table
       opts.setup = opts.setup or {}
 
-      -- 1) only start Volar in Vue/Nuxt projects
-      opts.setup["volar"] = function(server, volar_opts)
-        if not is_vue_project(vim.loop.cwd()) then
-          -- skip default volar.setup()
+      local util = require("lspconfig.util")
+      local is_vue_project = util.root_pattern("nuxt.config.ts", "nuxt.config.js", "vue.config.js")
+
+      -- only start Volar in Vue/Nuxt roots
+      opts.setup["volar"] = function(_, volar_opts)
+        if is_vue_project(volar_opts.root_dir) then
           return true
         end
-        -- else fall through and let LazyVim do the normal volar setup
+
+        volar_opts.handlers = volar_opts.handlers or {}
+        volar_opts.handlers["textDocument/definition"] = filter_dts
       end
 
-      -- 2) only start ts_ls in non-Vue projects
-      opts.setup["ts_ls"] = function(server, ts_opts)
-        if is_vue_project(vim.loop.cwd()) then
-          -- skip default ts_ls.setup()
+      -- only start tsserver in non-Vue roots
+      opts.setup["ts_ls"] = function(_, ts_opts)
+        if not is_vue_project(ts_opts.root_dir) then
           return true
         end
-        -- else normal ts_ls setup
+
+        ts_opts.handlers = ts_opts.handlers or {}
+        ts_opts.handlers["textDocument/definition"] = filter_dts
       end
 
-      -- 3) if you still have vtsls around, you can also disable it in pure-TS
-      opts.setup["vtsls"] = function(server, vtsls_opts)
-        if not is_vue_project(vim.loop.cwd()) then
+      -- only start vtsls in Vue roots
+      opts.setup["vtsls"] = function(_, vtsls_opts)
+        if not is_vue_project(vtsls_opts.root_dir) then
           return true
         end
       end
+
+      opts.servers = {
+        volar = {
+          on_attach = function(client, bufnr)
+            -- Disable formatting capability for Volar to prevent slowness on save
+            client.server_capabilities.documentFormattingProvider = false
+            client.server_capabilities.documentRangeFormattingProvider = false
+          end,
+        },
+        ts_ls = {
+          init_options = {
+            plugins = {
+              {
+                name = "@vue/typescript-plugin",
+                location = vim.fn.stdpath("data")
+                  .. "/mason/packages/vue-language-server/node_modules/@vue/language-server",
+                languages = { "vue" },
+              },
+            },
+          },
+          filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+        },
+        vtsls = {},
+      }
 
       return opts
     end,
